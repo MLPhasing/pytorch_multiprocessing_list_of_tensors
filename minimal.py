4502ed996953342250e27a02601c2b59a46b213e
Marc Päpper <marc@paepper.com>
2020-05-20 10:39:29 +0200

    Removed numpy

--- minimal.py
+++ minimal.py
@@ -1,5 +1,4 @@
 import pytorch_lightning as pl
-import numpy as np
 import torch
 from torch.utils.data import Dataset
 from torch.nn import functional as F
@@ -7,17 +6,10 @@ from torch.utils.data import DataLoader
 import argparse
 
 class FakeDataset(Dataset):
-    def __init__(self, use_numpy):
-        self.use_numpy = use_numpy
-        print('Using numpy: {}'.format(use_numpy))
-        if use_numpy:
-            self.list = np.random.random((20000, 3, 100, 100)).astype(np.float32)
-            self.length = 20000
-            print("size in nbytes: {}".format(self.list.nbytes))
-        else:
-            self.list = torch.rand((20000, 3, 100, 100))
-            self.length = 20000
-            print("size in nbytes: {}".format(self.list.numel() * self.list.element_size()))
+    def __init__(self):
+        self.list = torch.rand((20000, 3, 100, 100))
+        self.length = 20000
+        print("size in nbytes: {}".format(self.list.numel() * self.list.element_size()))
                 
     def __len__(self):
         return self.length
@@ -27,9 +19,8 @@ class FakeDataset(Dataset):
 
 
 class LightningWrapper(pl.LightningModule):
-    def __init__(self, use_numpy, num_workers):
+    def __init__(self, num_workers):
         super(LightningWrapper, self).__init__()
-        self.use_numpy = use_numpy
         self.num_workers = num_workers
         self.batch_size = batch_size
         self.layer_1 = torch.nn.Conv2d(3, 20, kernel_size=3)
@@ -54,7 +45,7 @@ class LightningWrapper(pl.LightningModule):
         return {'loss': loss}
     
     def prepare_data(self):
-        self.training_set = FakeDataset(use_numpy=self.use_numpy)
+        self.training_set = FakeDataset()
 
     def train_dataloader(self):
         print('Number of workers: {}, batch_size: {}'.format(self.num_workers, self.batch_size))
@@ -67,14 +58,13 @@ class LightningWrapper(pl.LightningModule):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Reconstruct memory overflow')
-    parser.add_argument('--numpy', action='store_true')
     parser.add_argument('--num_workers', type=int, default='10')
     parser.add_argument('--batch_size', type=int, default='128')
     parser.add_argument('--gpus', type=int, default='1')
     parser.add_argument('--backend', type=str, default='ddp')
     args = parser.parse_args()
     print(args)
-    lightning_model = LightningWrapper(args.numpy, args.num_workers, args.batch_size)
+    lightning_model = LightningWrapper(args.num_workers, args.batch_size)
     trainer = pl.Trainer(gpus=args.gpus, distributed_backend=args.backend, num_sanity_val_steps=0, profiler=True, max_epochs=5)
     start = time.time()
     trainer.fit(lightning_model)
